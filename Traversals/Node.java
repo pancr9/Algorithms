3c7825ae4110866a2b10abc6c1056ca858518a11
Rekhansh Panchal <rekhanshpanchal@gmail.com>
2018-03-09 13:58:56 -0500

    Create Node.java

---
+++ Traversals/Node.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Model class for a node in binary tree.
+ */
+class Node {
+
+    char key;
+    Node left, right;
+
+    Node(char key) {
+        this.key = key;
+    }
+}
